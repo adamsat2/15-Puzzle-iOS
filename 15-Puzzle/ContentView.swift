a4176c6110a3ac53fe60e40cbe3b0871051f77b1
adamsat2 <adamsatran@gmail.com>
2026-09-06 15:27:55 +0300

    Initial Commit

---
+++ 15-Puzzle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  15-Puzzle
+//
+//  Created by Adam Stern on 06/09/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 15-Puzzle/_5_PuzzleApp.swift
@@ -0,0 +1,17 @@
+//
+//  _5_PuzzleApp.swift
+//  15-Puzzle
+//
+//  Created by Adam Stern on 06/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct _5_PuzzleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
