8428d38c6059f5618419db98b12e4db941573504
adamsat2 <adamsatran@gmail.com>
2026-09-16 01:37:24 +0300

    Added TimeManager as a separate Objective C module.

---
+++ 15-Puzzle/Utils/TimeManager.h
@@ -0,0 +1,16 @@
+//
+//  TimeManager.h
+//  15-Puzzle
+//
+//  Created by Adam Stern on 16/09/2026.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TimeManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ 15-Puzzle/Utils/TimeManager.m
@@ -0,0 +1,12 @@
+//
+//  TimeManager.m
+//  15-Puzzle
+//
+//  Created by Adam Stern on 16/09/2026.
+//
+
+#import "TimeManager.h"
+
+@implementation TimeManager
+
+@end
